435a66a3c1789ce61404e4630f3ba888be642186
LukeMendes <120316098+LukeMendes@users.noreply.github.com>
2022-12-15 11:41:58 +0100

    Update SQL-Erweiterung.sql

--- SQL-Erweiterung.sql
+++ SQL-Erweiterung.sql
@@ -1,14 +1,18 @@
+/*alle Aufträge des Kunden "Maier GmbH"*/
 SELECT Auftrag.AuftragsNr, Auftrag.Datum FROM Auftrag, Kunde
 WHERE Auftrag.KundenNr = Kunde.KundenNr && Kunde.name = "Maier GmbH";
 
+/*Kundennummer, Namen der Kunden und die Anzahl der Aufträge zeigen*/
 SELECT Kunde.KundenNr, Kunde.Name, COUNT(*) FROM Kunde, Auftrag
 WHERE Kunde.KundenNr = Auftrag.KundenNr GROUP BY Kunde.KundenNr;
 
+/*m:n Beziehung als relationales Modell*/
 Kunde (KundenNr PRIMARY KEY, Name, Strasse, PLZ, Ort)
 Auftrag (AuftragsNr PRIMARY KEY , Datum, KundenNr FOREIGN KEY)
 Komponente(ArtikelNr PRIMARY KEY, Bezeichnung, Verkaufspreis)
 gilt_für(ArtikelNr PRIMARY KEY FOREIGN KEY, AuftragsNr PRIMARY KEY FOREIGN KEY, Anzahl)
 
+/*Kundennummer und den Gesamtumsatz durch diesen Kunden zeigen*/
 SELECT Kunde.KundenNr, SUM(Komponente.Verkaufspreis * gilt_für.Anzahl) AS "Umsatz"
 FROM Kunde, Auftrag, gilt_für, Komponente
 WHERE Kunde.KundenNr = Auftrag.KundenNr &&
